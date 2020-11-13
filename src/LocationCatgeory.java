cd45717785c2b53d8d7d97adc7ee00de126a04a0
Jim Ryan <jimmih15@gmail.com>
2020-11-13 19:43:21 +0000

    Upload property class, location category enum and bare-bones owner class

---
+++ src/LocationCatgeory.java
@@ -0,0 +1,3 @@
+public enum LocationCatgeory {
+    CITY, LARGETOWN, SMALLTOWN, VILLAGE, COUNTRYSIDE
+}
---
+++ src/Property.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+
+public class Property {
+    private ArrayList<Owner> owners;
+    private String address;
+    private String eircode;
+    private double estimatedMarketValue;
+    private LocationCatgeory locationCatgeory;
+    private boolean isPrincipalPrivateResidence;
+
+    public Property(ArrayList<Owner> owners, String address, String eircode, double estimatedMarketValue,
+            LocationCatgeory locationCatgeory, boolean isPrincipalPrivateResidence) {
+        this.setOwners(owners);
+        this.setAddress(address);
+        this.setEircode(eircode);
+        this.setEstimatedMarketValue(estimatedMarketValue);
+        this.setLocationCatgeory(locationCatgeory);
+        this.setPrincipalPrivateResidence(isPrincipalPrivateResidence);
+    }
+
+    public boolean isPrincipalPrivateResidence() {
+        return isPrincipalPrivateResidence;
+    }
+
+    public void setPrincipalPrivateResidence(boolean isPrincipalPrivateResidence) {
+        this.isPrincipalPrivateResidence = isPrincipalPrivateResidence;
+    }
+
+    public LocationCatgeory getLocationCatgeory() {
+        return locationCatgeory;
+    }
+
+    public void setLocationCatgeory(LocationCatgeory locationCatgeory) {
+        this.locationCatgeory = locationCatgeory;
+    }
+
+    public double getEstimatedMarketValue() {
+        return estimatedMarketValue;
+    }
+
+    public void setEstimatedMarketValue(double estimatedMarketValue) {
+        this.estimatedMarketValue = estimatedMarketValue;
+    }
+
+    public String getEircode() {
+        return eircode;
+    }
+
+    public void setEircode(String eircode) {
+        this.eircode = eircode;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public ArrayList<Owner> getOwners() {
+        return owners;
+    }
+
+    public void setOwners(ArrayList<Owner> owners) {
+        this.owners = owners;
+    }
+
+}
