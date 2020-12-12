23d8650b417aa4a70eaae423c986d72b043074e1
faad555 <60967843+faad555@users.noreply.github.com>
2020-12-12 17:08:04 +0000

    Update with Javadoc comments

--- src/Owner.java
+++ src/Owner.java
@@ -1,4 +1,11 @@
 import java.util.ArrayList;
+
+
+/**
+ * A Class Representing Owner's Object
+ * @author User-1
+ * @version 1.0.0
+ */
 public class Owner {
 
     private String name = "";
@@ -6,53 +13,91 @@ public class Owner {
     private String password = "";
     private ArrayList<String> propertiesOwned = new ArrayList<String>();
 
+    /**
+     * @param name Name of owner
+     * @param ppsNum Personal Public Service Number (ppsNum) of owner
+     * @param password Password of owner
+     */
     public Owner(String name, String ppsNum, String password) {
-    
         setName(name);
         setPpsNum(ppsNum);
         setPassword(password);
     }
-	//method to set name 
+
+
+    /**
+     * Method to set name of owner
+     * @param name name of the owner
+     */
     public void setName(String name) {
         this.name = name;
     }
 
-	//method to set pps number
+    /**
+     * Method to set pps number 
+     * @param ppsNum Personal Public Service Number of owner
+     */
     public void setPpsNum(String ppsNum) {
         this.ppsNum = ppsNum;
     }
 
-	//method to set password (no return type)
+
+    /**
+     * Method to set password (no return type)
+     * @param password Password of the wner
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
-	//method to return name
+
+    /**
+     * Method to return name
+     * @return value of name
+     */
     public String getName() {
         return name;
     }
-    
-    //method to show ppsNum number
+
+
+    /**
+     * Personal Public Service Number
+     * @return return Personal Public Service Number of the owner
+     */
     public String getPpsNum() {
         return ppsNum;
     }
-    
-    //Show password
+
+
+    /**
+     * Show password
+     * @return password of user
+     */
     public String getPassword() {
         return password;
     }
 
-    //Add Property
+    /**
+     * Add Property to Owner's property list
+     * @param eircode eircode of Property
+     */
     public void addProperty(String eircode){
         propertiesOwned.add(eircode);
     }
 
-    //method to Remove Property from the list 
+    /**
+     * Method to Remove Property from the list using id
+     * @param propertyId id of Property
+     */
     public void removeProperty(int propertyId){
         propertiesOwned.remove(propertyId);
     }
 
-    //Get properties ID
+
+    /**
+     * Get properties list
+     * @return List of properties Owned
+     */
     public ArrayList<String> getPropertiesEircodes(){
         return propertiesOwned;
     }
