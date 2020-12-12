4192f6062fe11b7b1c7e90737347d3af8d7c78d3
TomC17 <tommiecarey17@gmail.com>
2020-12-12 23:19:30 +0000

    Update CliMenu.java
    Co-Authored-By: Jim Ryan <jimmih15@gmail.com>
    Co-Authored-By: Harn33t <58052846+Harn33t@users.noreply.github.com>
    Co-Authored-By: Fawad Shahzad <60967843+faad555@users.noreply.github.com>

--- src/CliMenu.java
+++ src/CliMenu.java
@@ -8,6 +8,7 @@ public class CliMenu {
     private Scanner in;
     private SystemManager sm = new SystemManager();
     private Owner owner;
+    private Employee employee;
 
     public CliMenu() {
         in = new Scanner(System.in);
@@ -28,7 +29,7 @@ public class CliMenu {
             } else if (command.equals("S")) {
                 signup();
             } else if (command.equals("D")) {
-                department();
+                depLogin();
             } else if (command.equals("Q")) {
                 more = false;
             }
@@ -98,7 +99,8 @@ public class CliMenu {
 
         while (more) {
             System.out.println("Welcome to your dashboard " + owner.getName());
-            System.out.println("R)egister Property  V)iew All Properties  P)ay Tax  L)ogout");
+            System.out.println(
+                    "R)egister Property  V)iew All Properties  P)ay Tax PR)evious Payments B)alancing Statement  L)ogout");
             String command = in.nextLine().toUpperCase();
 
             if (command.equals("R")) {
@@ -107,14 +109,14 @@ public class CliMenu {
                 view();
             } else if (command.equals("P")) {
                 pay();
+            } else if (command.equals("PR")) {
+                prevPayments();
+            } else if (command.equals("B")) {
+                balStatement();
             } else if (command.equals("L")) {
                 more = false;
             }
         }
-    }
-
-    public void department() {
-        System.out.println("Department Menu");
     }
 
     public void register() {
@@ -136,64 +138,67 @@ public class CliMenu {
         String address = in.nextLine();
         System.out.println("What is the eircode?");
         String eircode = in.nextLine().trim().toUpperCase();
-        System.out.println("What is the estimated Market value? Only input digit characters");
-        String estimatedMarketValue = in.nextLine();
+        if (!sm.isValidEircode(eircode)) {
+            System.out.println("Not a valid Eircode!");
+        } else {
+            System.out.println("What is the estimated Market value? Only input digit characters");
+            String estimatedMarketValue = in.nextLine();
 
-        String locationCategory = "";
+            String locationCategory = "";
 
-        boolean more = true;
+            boolean more = true;
 
-        while (more) {
-            more = false;
-            System.out.println("What is the location category");
-            System.out.println("CI)ty L)arge Town S)mall Town V)illage CO)untryside");
-            String command = in.nextLine().toUpperCase();
+            while (more) {
+                more = false;
+                System.out.println("What is the location category");
+                System.out.println("CI)ty L)arge Town S)mall Town V)illage CO)untryside");
+                String command = in.nextLine().toUpperCase();
 
-            if (command.equals("CI")) {
-                locationCategory = "City";
+                if (command.equals("CI")) {
+                    locationCategory = "City";
 
-            } else if (command.equals("L")) {
-                locationCategory = "Large Town";
-            } else if (command.equals("S")) {
-                locationCategory = "Small Town";
-            } else if (command.equals("V")) {
-                locationCategory = "Village";
-            } else if (command.equals("CO")) {
-                locationCategory = "Countryside";
-            } else {
-                more = true;
+                } else if (command.equals("L")) {
+                    locationCategory = "Large Town";
+                } else if (command.equals("S")) {
+                    locationCategory = "Small Town";
+                } else if (command.equals("V")) {
+                    locationCategory = "Village";
+                } else if (command.equals("CO")) {
+                    locationCategory = "Countryside";
+                } else {
+                    more = true;
+                }
+            }
+            System.out.println("Is this the principal private residence? y/n");
+            ans = in.nextLine().trim().toUpperCase();
+            boolean isPrincipalPrivateResidence = false;
+            if (ans.equals("Y")) {
+                isPrincipalPrivateResidence = true;
+            }
+            System.out.println("");
+            System.out.println("Review of inputted data");
+            System.out.println("Additional Owners: " + additionalOwners);
+            System.out.println("Address: " + address);
+            System.out.println("Eircode: " + eircode);
+            System.out.println("Estimated Market Value: " + estimatedMarketValue);
+            System.out.println("Location Category: " + locationCategory);
+            System.out.println("Is principal private residence: " + isPrincipalPrivateResidence);
+            System.out.println("");
+            System.out.println("Would you like to register this property? y/n");
+            ans = in.nextLine().trim().toUpperCase();
+            if (ans.equals("Y")) {
+                System.out.println("Property Registered");
+                int year = Calendar.getInstance().get(Calendar.YEAR);
+                StringBuilder sb = new StringBuilder();
+                sb.append(owner.getPpsNum());
+                for (String s : additionalOwners) {
+                    sb.append(", ");
+                    sb.append(s);
+                }
+                sm.registerProperty(year, sb.toString(), address, eircode, estimatedMarketValue, locationCategory,
+                        isPrincipalPrivateResidence);
             }
         }
-        System.out.println("Is this the principal private residence? y/n");
-        ans = in.nextLine().trim().toUpperCase();
-        boolean isPrincipalPrivateResidence = false;
-        if (ans.equals("Y")) {
-            isPrincipalPrivateResidence = true;
-        }
-        System.out.println("");
-        System.out.println("Review of inputted data");
-        System.out.println("Additional Owners: " + additionalOwners);
-        System.out.println("Address: " + address);
-        System.out.println("Eircode: " + eircode);
-        System.out.println("Estimated Market Value: " + estimatedMarketValue);
-        System.out.println("Location Category: " + locationCategory);
-        System.out.println("Is principal private residence: " + isPrincipalPrivateResidence);
-        System.out.println("");
-        System.out.println("Would you like to register this property? y/n");
-        ans = in.nextLine().trim().toUpperCase();
-        if (ans.equals("Y")) {
-            System.out.println("Property Registered");
-            int year = Calendar.getInstance().get(Calendar.YEAR);
-            StringBuilder sb = new StringBuilder();
-            sb.append(owner.getPpsNum());
-            for (String s : additionalOwners) {
-                sb.append(", ");
-                sb.append(s);
-            }
-            sm.registerProperty(year, sb.toString(), address, eircode, estimatedMarketValue, locationCategory,
-                    isPrincipalPrivateResidence);
-        }
-
     }
 
     public void view() {
@@ -203,18 +208,22 @@ public class CliMenu {
         System.out.println("Please enter the Eircode of the property you wish to view the details of.");
         String eircode = in.nextLine().trim().toUpperCase();
         // Do a check to return a balance for a specific eircode
-        if (eircode == null) {
-            System.out.println("No Eircode Entered.");
-        } else if (properties.contains(eircode)) {
-            Property p = sm.getPropertyData(eircode);
-            String txtEircode = p.getEircode();
-            String txtAddrs = p.getAddress();
-            String txtLocationCategory = p.getLocationCatgeory();
-            String txtEstMarketValue = p.getEstimatedMarketValue();
-            System.out.println("Eircode: " + txtEircode + "\nAddress: " + txtAddrs + "\nLocation Category: "
-                    + txtLocationCategory + "\nEstimated Market Value: " + txtEstMarketValue);
+        if (!sm.isValidEircode(eircode)) {
+            System.out.println("Not a valid Eircode!");
         } else {
-            System.out.println("Eircode not listed!");
+            if (eircode == null) {
+                System.out.println("No Eircode Entered.");
+            } else if (properties.contains(eircode)) {
+                Property p = sm.getPropertyData(eircode);
+                String txtEircode = p.getEircode();
+                String txtAddrs = p.getAddress();
+                String txtLocationCategory = p.getLocationCatgeory();
+                String txtEstMarketValue = p.getEstimatedMarketValue();
+                System.out.println("Eircode: " + txtEircode + "\nAddress: " + txtAddrs + "\nLocation Category: "
+                        + txtLocationCategory + "\nEstimated Market Value: " + txtEstMarketValue);
+            } else {
+                System.out.println("Eircode not listed!");
+            }
         }
     }
 
@@ -224,23 +233,187 @@ public class CliMenu {
         System.out.println(Arrays.deepToString(properties.toArray()));
         System.out.println("Please enter the Eircode of the property you wish to pay tax for.");
         String eircode = in.nextLine().trim().toUpperCase();
-        LocalDateTime year = LocalDateTime.now();
-        // Do a check to return a balance for a specific eircode
-        if (eircode == null) {
-            System.out.println("No Eircode Entered.");
-        } else if (properties.contains(eircode)) {
-            double bal = sm.calculateTax(eircode);
-            System.out.println("Balance Due: " + bal);
-            System.out.println("Pay balance? y/n");
-            String ans = in.nextLine().trim().toLowerCase();
-            if (ans.equals("y")) {
-                System.out.println("Balance paid!");
-                sm.makePayment(year.getYear(), eircode);
-            } else {
-                System.out.println("Balance not paid!");
-            }
+        if (!sm.isValidEircode(eircode)) {
+            System.out.println("Not a valid Eircode!");
         } else {
-            System.out.println("Eircode not listed!");
+            LocalDateTime year = LocalDateTime.now();
+            // Do a check to return a balance for a specific eircode
+            if (eircode == null) {
+                System.out.println("No Eircode Entered.");
+            } else if (properties.contains(eircode)) {
+                double bal = sm.calculateTax(eircode);
+                System.out.println("Balance Due: " + bal);
+                System.out.println("Pay balance? y/n");
+                String ans = in.nextLine().trim().toLowerCase();
+                if (ans.equals("y")) {
+                    System.out.println("Balance paid!");
+                    sm.makePayment(year.getYear(), eircode);
+                } else {
+                    System.out.println("Balance not paid!");
+                }
+            } else {
+                System.out.println("Eircode not listed!");
+            }
         }
     }
+
+    public void prevPayments() {
+        System.out.println("Previous Payments");
+        ArrayList<Record> paidRecords = new ArrayList<Record>();
+        ArrayList<String> eircodes = sm.getOwnerPropertiesEircodes(owner.getPpsNum());
+        for (String eircode : eircodes) {
+            ArrayList<Record> recordsTemp = sm.getPaymentRecords(eircode);
+            for (Record r : recordsTemp) {
+                if (r.getPaymentStatus().equalsIgnoreCase("paid")) {
+                    paidRecords.add(r);
+                }
+            }
+        }
+        if (paidRecords.size() == 0) {
+            System.out.println("You have no payment records");
+        }
+        for (int i = 0; i < paidRecords.size(); i++) {
+            Record r = paidRecords.get(i);
+            System.out.println("Record " + (i + 1));
+            System.out.println("Eircode: " + r.getEircode());
+            System.out.println("Status: " + r.getPaymentStatus());
+            System.out.println("Tax Amount: " + r.getTaxAmount());
+            System.out.println("Year of Payment: " + r.getYear());
+        }
+    }
+
+    public void balStatement() {
+        System.out.println("Balancing Statement");
+        ArrayList<Record> unpaidRecords = new ArrayList<Record>();
+        ArrayList<String> eircodes = sm.getOwnerPropertiesEircodes(owner.getPpsNum());
+        for (String eircode : eircodes) {
+            ArrayList<Record> recordsTemp = sm.getPaymentRecords(eircode);
+            for (Record r : recordsTemp) {
+                if (!r.getPaymentStatus().equalsIgnoreCase("paid")) {
+                    unpaidRecords.add(r);
+                }
+            }
+        }
+        if (unpaidRecords.size() == 0) {
+            System.out.println("You have no outstanding tax.");
+        }
+        for (int i = 0; i < unpaidRecords.size(); i++) {
+            Record r = unpaidRecords.get(i);
+            System.out.println("Record " + (i + 1));
+            System.out.println("Eircode: " + r.getEircode());
+            System.out.println("Status: " + r.getPaymentStatus());
+            System.out.println("Tax Amount: " + r.getTaxAmount());
+        }
+    }
+
+    // Department Side
+    public void depLogin() {
+        System.out.println("Department Login Menu");
+        System.out.println("Enter your Worker ID: ");
+        String wid = in.nextLine().trim().toUpperCase();
+        if (!sm.isValidWID(wid)) {
+            System.out.println("This is not a valid Worker ID");
+        } else if (!sm.employeeExists(wid)) {
+            System.out.println(
+                    "This Worker ID number is not associated with an account. Would you like to register? y/n");
+            String ans = in.nextLine().trim().toLowerCase();
+            if (ans.equals("y")) {
+                depSignup();
+            } else {
+                run();
+            }
+        }
+        System.out.println("Enter your password");
+        String password = in.nextLine();
+
+        while (!sm.depLoginVerification(wid, password)) {
+            System.out.println("This password is incorrect. Would you like to try again? y/n");
+            String ans = in.nextLine().trim().toLowerCase();
+            if (!ans.equals("y")) {
+                run();
+            }
+            password = in.nextLine();
+        }
+        depDash();
+    }
+
+    public void depSignup() {
+        System.out.println("Department Registration Menu");
+        System.out.println("Enter your Work ID: ");
+        String wid = in.nextLine().trim().toUpperCase();
+        if (sm.employeeExists(wid)) {
+            System.out.println("User already exists! Please log in!");
+        } else if (!sm.isValidWID(wid)) {
+            System.out.println("This is not a valid Worker ID number");
+        } else {
+            System.out.println("Enter your name");
+            String name = in.nextLine().trim();
+            System.out.println("Enter your password");
+            String password = in.nextLine().trim();
+            if (sm.isValidPassword(password)) {
+                sm.registerEmployee(name, wid, password);
+                System.out.println("You have registered successfully!");
+                employee = sm.getEmployee(wid);
+                depDash();
+            } else {
+                System.out.println("Your password is invalid please try again.");
+            }
+        }
+    }
+
+    public void depDash() {
+        boolean more = true;
+
+        while (more) {
+            System.out.println("Welcome to your work dashboard " + employee.getName());
+            System.out.println("O)verdue Properties T)ax Statistics by Area  P)roperty Tax Payment Data L)ogout");
+            String command = in.nextLine().toUpperCase();
+
+            if (command.equals("O")) {
+                viewOverdueProp();
+            } else if (command.equals("T")) {
+                PropTaxStats();
+            } else if (command.equals("P")) {
+                ViewPropTaxData();
+            } else if (command.equals("L")) {
+                more = false;
+            }
+        }
+    }
+
+    public void viewOverdueProp() {
+        System.out.println("Overdue Properties");
+        ArrayList<Record> unpaidRecords = new ArrayList<Record>();
+        System.out.println("Please enter an Eircode Prefix e.g. V42");
+        String eirRoutingKey = in.nextLine().trim();
+        System.out.println("Please enter a year.");
+        int year = in.nextInt();
+        ArrayList<Record> overdueProperties = sm.getAllOverDueProps(year, eirRoutingKey);
+        for (Record r : overdueProperties) {
+            if (!r.getPaymentStatus().equalsIgnoreCase("paid")) {
+                unpaidRecords.add(r);
+            }
+            if (unpaidRecords.size() == 0) {
+                System.out.println("There are no Overdue Properties.");
+            } else {
+                for (int i = 0; i < unpaidRecords.size(); i++) {
+                    Record k = unpaidRecords.get(i);
+                    System.out.println("Record " + (i + 1));
+                    System.out.println("Eircode: " + k.getEircode());
+                    System.out.println("Status: " + k.getPaymentStatus());
+                    System.out.println("Tax Amount: " + k.getTaxAmount());
+                }
+            }
+        }
+
+    }
+
+    public void PropTaxStats() {
+        System.out.println("Property Tax Statistics By Area");
+    }
+
+    public void ViewPropTaxData() {
+        System.out.println("Property Tax Payment Data");
+    }
+
 }
